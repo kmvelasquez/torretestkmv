cb178b6d4ba7966a05d2f004d8b548fe72e914de
Katherene Velasquez <kumichelleknight44@gmail.com>
2023-04-15 00:13:19 -0600

    1 - First Migration and Models

--- skills/models.py
+++ skills/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#User table already exists in django
+
+class Skills(models.Model):
+    skillname = models.CharField(max_length=100,blank = False, null = False)
+    
+    def __str__(self):
+        return str(self.Skills)
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100,blank = False, null = False)
+    role = models.CharField(max_length=100,blank = False, null = False)
+    role =models.CharField(max_length=100, blank = False, null = False)
+    start_date = models.DateTimeField(blank=False,null=False)
+    end_date = models.DateTimeField(blank=False,null=False)
+
+    def __str__(self):
+        return str(self.Experience)
+
